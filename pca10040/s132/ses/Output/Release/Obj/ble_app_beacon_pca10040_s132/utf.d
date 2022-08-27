a7537e7c01005626971cb087383354b1ec74ff0c
AliJOUBIR <93798266+AliJOUBIR@users.noreply.github.com>
2022-08-27 12:46:50 +0100

    init project, based on nrf5_sdk_17.1.0_ddde560 ble_app_beacon example

---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_beacon_pca10040_s132/utf.d
@@ -0,0 +1,9 @@
+Output/Release/Obj/ble_app_beacon_pca10040_s132/utf.o: \
+ C:\Users\ALI_JOUBIR\ncs\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.c \
+ C:\Users\ALI_JOUBIR\ncs\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.h \
+ C:/Users/ALI_JOUBIR/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stddef.h \
+ C:/Users/ALI_JOUBIR/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/__config \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.66/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.66/include/__crossworks.h \
+ C:/Users/ALI_JOUBIR/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.66/include/stdint.h
